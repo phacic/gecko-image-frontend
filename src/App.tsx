11e73ae7f64755aadb1956a6257ae534d2c1f1a3
Karlmichael <karlmichael@mpharma.com>
2022-01-08 07:49:45 +0000

    added page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,55 @@
 import './App.css'
 
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [selectedImage, setSelectedImage] = useState<any>()
+
+  const onImageFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files)
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0])
+
+      console.log('selected image', selectedImage)
+    }
+  }
 
   return (
-    <div className="App" data-testid="app-page">
-      <header className="App-header">
-        <p className="header">
-          🚀 Vite + React + Typescript + React Testing Library 🤘 <br />& Eslint
-          🔥+ Prettier
-        </p>
+    <div className="page">
+      <div>
+        <h1 className="text-4xl py-5 font-bold">Upload an Image</h1>
+        <p className="separator-text">Either from you local device</p>
 
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p>
-            {' '}
-            Don&apos;t forgot to install Eslint and Prettier in Your Vscode.
-          </p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer">
-              Vite Docs
-            </a>
-          </p>
+        <div className="flex flex-row gap-3 content-center items-center">
+          <label className="block">
+            <span className="sr-only">Select image</span>
+            <input
+              className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold
+                      file:bg-violet-100 file:text-violet-700 hover:file:bg-violet-100"
+              type={'file'}
+              accept="image/*"
+              onChange={onImageFileChange}
+            />
+          </label>
+          <div>
+            <button className="btn">upload</button>
+          </div>
         </div>
-      </header>
+
+        <p className="separator-text">Or from a source url.</p>
+        <div className="flex flex-row gap-3 content-center items-center">
+          <div className="basis-3/4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text" placeholder="Image url"
+            />
+          </div>
+          <div className="basis-1/4">
+            <button className="btn">upload</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
